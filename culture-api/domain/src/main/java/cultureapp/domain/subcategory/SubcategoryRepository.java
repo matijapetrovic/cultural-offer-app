13c408aebaa7a6a534ea6ee694ea3a05ef26dce9
Matija Petrovic <matijapetrovicweb@gmail.com>
2020-11-26 15:28:08 +0100

    Added API CRUD operations for subcategories

--- culture-api/domain/src/main/java/cultureapp/domain/subcategory/SubcategoryRepository.java
+++ culture-api/domain/src/main/java/cultureapp/domain/subcategory/SubcategoryRepository.java
@@ -4,9 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface SubcategoryRepository extends JpaRepository<Subcategory, SubcategoryId> {
 
-    List<Subcategory> findAllByCategoryId(Long categoryId);
+    List<Subcategory> findAllByCategoryIdAndArchivedFalse(Long categoryId);
+    Optional<Subcategory> findByCategoryIdAndIdAndArchivedFalse(Long categoryId, Long id);
 }
