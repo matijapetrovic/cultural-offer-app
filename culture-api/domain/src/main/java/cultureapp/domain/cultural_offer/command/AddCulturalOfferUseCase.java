57d45b02afa1f4234b657d48015665ee27ab702b
bodroza.joca <bodroza.joca@gmail.com>
2020-12-02 18:47:28 +0100

    Constraints fixed

--- culture-api/domain/src/main/java/cultureapp/domain/cultural_offer/command/AddCulturalOfferUseCase.java
+++ culture-api/domain/src/main/java/cultureapp/domain/cultural_offer/command/AddCulturalOfferUseCase.java
@@ -6,7 +6,6 @@ import lombok.EqualsAndHashCode;
 import lombok.Value;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import java.util.List;
 
@@ -21,7 +20,7 @@ public interface AddCulturalOfferUseCase {
         String name;
         
         Double longitude;
-        
+
         Double latitude;
 
         List<byte[]> images;
