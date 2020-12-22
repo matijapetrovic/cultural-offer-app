a63abb856a07318341e28cabb5c42ada7d756147
Matija Petrovic <matijapetrovicweb@gmail.com>
2020-12-22 16:05:20 +0100

    Added filter query for cultural offer locations

---
+++ culture-api/domain/src/main/java/cultureapp/domain/cultural_offer/exception/CulturalOfferLocationsFilterException.java
@@ -0,0 +1,7 @@
+package cultureapp.domain.cultural_offer.exception;
+
+public class CulturalOfferLocationsFilterException extends Exception {
+    public CulturalOfferLocationsFilterException(String msg) {
+        super(msg);
+    }
+}
--- culture-api/rest/src/main/java/cultureapp/rest/core/error/RestExceptionHandler.java
+++ culture-api/rest/src/main/java/cultureapp/rest/core/error/RestExceptionHandler.java
@@ -1,5 +1,6 @@
 package cultureapp.rest.core.error;
 import cultureapp.domain.account.exception.AccountAlreadyExistsException;
+import cultureapp.domain.cultural_offer.exception.CulturalOfferLocationsFilterException;
 import cultureapp.domain.user.exception.AdminNotFoundException;
 import cultureapp.domain.category.exception.CategoryAlreadyExistsException;
 import cultureapp.domain.category.exception.CategoryNotFoundException;
@@ -27,6 +28,14 @@ import java.io.IOException;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice("cultureapp.rest")
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(CulturalOfferLocationsFilterException.class)
+    protected ResponseEntity<Object> handleCulturalOfferLocationsFilterException(
+            CulturalOfferLocationsFilterException ex
+    ) {
+        ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, ex.getMessage(), ex);
+        return buildResponse(apiError);
+    }
+
     @ExceptionHandler(SubscriptionNotFoundException.class)
     protected ResponseEntity<Object> handleSubscriptionNotFound(
             SubscriptionNotFoundException ex) {
