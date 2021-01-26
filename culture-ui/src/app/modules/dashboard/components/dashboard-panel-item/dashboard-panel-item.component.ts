9a3a13a332e4b17440b56890fa9ae9c991a469b2
Nemanja Jevtić <njevtic22@gmail.com>
2021-01-26 02:01:18 +0100

    Implemented dashboard.

--- culture-ui/src/app/modules/dashboard/components/dashboard-panel-item/dashboard-panel-item.component.ts
+++ culture-ui/src/app/modules/dashboard/components/dashboard-panel-item/dashboard-panel-item.component.ts
@@ -1,4 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { CulturalOffersService } from 'src/app/modules/cultural-offers/cultural-offers.service';
 import { SubscribedOffer } from '../../subscriptions-details';
 
 @Component({
@@ -9,9 +12,47 @@ import { SubscribedOffer } from '../../subscriptions-details';
 export class DashboardPanelItemComponent implements OnInit {
   @Input() subscribedOffer: SubscribedOffer;
 
-  constructor() { }
+  @Output() unsubscribedEvent: EventEmitter<void> = new EventEmitter<void>();
+
+  constructor(
+    private router: Router,
+    private offersService: CulturalOffersService,
+    private messageService: MessageService,
+    private confirmationService: ConfirmationService
+  ) { }
 
   ngOnInit(): void {
+    // this.unsubscribedEvent = new EventEmitter<void>();
   }
 
+  redirectToOfferPage(): void {
+    this.router.navigate([`/cultural-offers/${this.subscribedOffer.id}`]);
+  }
+
+  confirmUnsubscribe(event: Event): void {
+    // this.confirmationService.confirm({
+    //   target: event.target,
+    //   message: 'Are you sure that you want to unsubscribe from this offer?',
+    //   icon: 'pi pi-exclamation-triangle',
+    //   accept: () => {
+    //     console.log('eeeeeeeeeeeeee');
+
+    //     this.unsubscribe();
+    //   },
+    //   reject: () => {
+    //     console.log("oooooooooooooo");
+    //   }
+    // });
+    this.unsubscribe();
+  }
+
+  unsubscribe(): void {
+    this.offersService.unsubscribeFromCulturalOffer(this.subscribedOffer.id)
+      .subscribe(() => {
+        this.unsubscribedEvent.emit();
+        this.messageService.add({ severity: 'success', summary: 'Unsubscription successful', detail: 'You have successfully unsubscribed from chosen cultural offer\'s newsletter' });
+        setTimeout(() => this.messageService, 5000);
+      });
+
+  }
 }
