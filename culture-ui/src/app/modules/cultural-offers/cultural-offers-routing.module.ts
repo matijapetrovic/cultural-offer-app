96dd41d5bd18b0f1f9af39acec49adcf45df42fe
nikolakabasaj <44526113+nikolakabasaj@users.noreply.github.com>
2021-01-27 22:14:31 +0100

    cleanup

--- culture-ui/src/app/modules/cultural-offers/cultural-offers-routing.module.ts
+++ culture-ui/src/app/modules/cultural-offers/cultural-offers-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from 'src/app/core/guards/auth.guard';
+import { Role } from '../authentication/role';
 import { CulturalOfferComponent } from './pages/cultural-offer/cultural-offer.component';
 import { CulturalOffersComponent } from './pages/cultural-offers/cultural-offers.component';
 
@@ -10,11 +12,15 @@ const routes: Routes = [
   },
   {
     path: ':offerId/news',
-    loadChildren: () => import('./../news/news.module').then(m => m.NewsModule)
+    loadChildren: () => import('./../news/news.module').then(m => m.NewsModule),
+    canActivate: [AuthGuard],
+    data: { roles: [Role.ROLE_ADMIN] }
   },
   {
     path: '',
-    component: CulturalOffersComponent
+    component: CulturalOffersComponent,
+    canActivate: [AuthGuard],
+    data: { roles: [Role.ROLE_ADMIN] }
   }
 ];
 
