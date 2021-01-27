572ed69402d50d4fe5eeb5b966356863729ed0fb
Nemanja Jevtić <njevtic22@gmail.com>
2021-01-27 07:06:44 +0100

    Implemented angular service tests

--- culture-ui/src/app/modules/reviews/reviews.service.spec.ts
+++ culture-ui/src/app/modules/reviews/reviews.service.spec.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
 import { fakeAsync, getTestBed, TestBed, tick } from '@angular/core/testing';
-import { mockReviewPage } from 'src/app/shared/testing/mock-data';
+import { mockReviewPage, mockEmptyReviewPage, mockReviewToAdd, mockEmptyCommentReviewToAdd, mockNegativeRatingReviewToAdd } from 'src/app/shared/testing/mock-data';
 import { environment } from 'src/environments/environment';
 import { ReviewPage } from './review';
 
@@ -12,6 +12,7 @@ describe('ReviewsService', () => {
   let injector: TestBed;
   let httpMock: HttpTestingController;
   let httpClient: HttpClient;
+  const offersUrl = `${environment.apiUrl}/api/cultural-offers`;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -40,9 +41,9 @@ describe('ReviewsService', () => {
     let reviewPage: ReviewPage;
     service.getReviews(culturalOfferId, page, limit).subscribe((result) => reviewPage = result);
 
-    const url = `${environment.apiUrl}/api/cultural-offers/${culturalOfferId}/reviews?page=${page}&limit=${limit}`;
+    const reviewsUrl = `${offersUrl}/${culturalOfferId}/reviews?page=${page}&limit=${limit}`;
 
-    const req = httpMock.expectOne(url);
+    const req = httpMock.expectOne(reviewsUrl);
     expect(req.request.method).toBe('GET');
     req.flush(mockReviewPage);
 
@@ -57,5 +58,229 @@ describe('ReviewsService', () => {
     expect(reviewPage.links.has('self')).toBeTrue();
     expect(reviewPage.links.has('next')).toBeTrue();
     expect(reviewPage.links.has('prev')).toBeFalse();
+  }));
+
+  it('getReviews() should not return any reviews => offer does not contain reviews', fakeAsync(() => {
+    // cultural offer with id 1000 does not have any reviews
+    const culturalOfferId = 1000;
+    const page = 0;
+    const limit = 5;
+
+    let reviewPage: ReviewPage;
+    service.getReviews(culturalOfferId, page, limit).subscribe(result => reviewPage = result);
+
+    const reviewsUrl = `${offersUrl}/${culturalOfferId}/reviews?page=${page}&limit=${limit}`;
+
+    const req = httpMock.expectOne(reviewsUrl);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockEmptyReviewPage);
+
+    tick();
+
+    expect(reviewPage).toBeDefined();
+    expect(reviewPage.data).toBeDefined();
+    expect(reviewPage.data.length).toEqual(mockEmptyReviewPage.data.length);
+
+    expect(reviewPage.links).toBeDefined();
+    expect(reviewPage.links.size).toEqual(mockEmptyReviewPage.links.size);
+    expect(reviewPage.links.has('prev')).toBeTrue();
+    expect(reviewPage.links.has('self')).toBeTrue();
+    expect(reviewPage.links.has('next')).toBeFalse();
+  }));
+
+  it('getReviews() should not return any reviews => empty page', fakeAsync(() => {
+    const culturalOfferId = 1;
+    // there are no reviews on page 1000
+    const page = 1000;
+    const limit = 5;
+
+    let reviewPage: ReviewPage;
+    service.getReviews(culturalOfferId, page, limit).subscribe(result => reviewPage = result);
+
+    const reviewsUrl = `${offersUrl}/${culturalOfferId}/reviews?page=${page}&limit=${limit}`;
+
+    const req = httpMock.expectOne(reviewsUrl);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockEmptyReviewPage);
+
+    tick();
+
+    expect(reviewPage).toBeDefined();
+    expect(reviewPage.data).toBeDefined();
+    expect(reviewPage.data.length).toEqual(mockEmptyReviewPage.data.length);
+
+    expect(reviewPage.links).toBeDefined();
+    expect(reviewPage.links.size).toEqual(mockEmptyReviewPage.links.size);
+    expect(reviewPage.links.has('prev')).toBeTrue();
+    expect(reviewPage.links.has('self')).toBeTrue();
+    expect(reviewPage.links.has('next')).toBeFalse();
+  }));
+
+  it('getReviews() should throw exception => non existing offer id', fakeAsync(() => {
+    // cultural offer with id -1 does not exist
+    const culturalOfferId = -1;
+    const page = 0;
+    const limit = 5;
+
+    const expectedResponse: HttpErrorResponse = new HttpErrorResponse({
+      error: 'Cultural offer does not exist',
+      status: 404,
+      statusText: 'Not Found'
+    });
+
+    let response: any;
+    service.getReviews(culturalOfferId, page, limit).subscribe(result => response = result);
+
+    const reviewsUrl = `${offersUrl}/${culturalOfferId}/reviews?page=${page}&limit=${limit}`;
+
+    const req = httpMock.expectOne(reviewsUrl);
+    expect(req.request.method).toBe('GET');
+    req.flush(expectedResponse);
+
+    tick();
+
+    expect(response.error).toEqual(expectedResponse.error);
+    expect(response.status).toEqual(expectedResponse.status);
+    expect(response.statusText).toEqual(expectedResponse.statusText);
+  }));
+
+  it('getReviews() should throw exception => invalid page number', fakeAsync(() => {
+    const culturalOfferId = 1;
+    const page = -1;
+    const limit = 5;
+
+    const expectedResponse: HttpErrorResponse = new HttpErrorResponse({
+      error: 'Invalid page number',
+      status: 400,
+      statusText: 'Bad request'
+    });
+
+    let response: any;
+    service.getReviews(culturalOfferId, page, limit).subscribe(result => response = result);
+
+    const reviewsUrl = `${offersUrl}/${culturalOfferId}/reviews?page=${page}&limit=${limit}`;
+    const req = httpMock.expectOne(reviewsUrl);
+    expect(req.request.method).toBe('GET');
+    req.flush(expectedResponse);
+
+    tick();
+
+    expect(response.error).toEqual(expectedResponse.error);
+    expect(response.status).toEqual(expectedResponse.status);
+    expect(response.statusText).toEqual(expectedResponse.statusText);
+  }));
+
+  it('getReviews() should throw exception => invalid limit number', fakeAsync(() => {
+    const culturalOfferId = 1;
+    const page = 1;
+    const limit = -5;
+
+    const expectedResponse: HttpErrorResponse = new HttpErrorResponse({
+      error: 'Invalid limit number',
+      status: 400,
+      statusText: 'Bad request'
+    });
+
+    let response: any;
+    service.getReviews(culturalOfferId, page, limit).subscribe(result => response = result);
+
+    const reviewsUrl = `${offersUrl}/${culturalOfferId}/reviews?page=${page}&limit=${limit}`;
+    const req = httpMock.expectOne(reviewsUrl);
+    expect(req.request.method).toBe('GET');
+    req.flush(expectedResponse);
+
+    tick();
+
+    expect(response.error).toEqual(expectedResponse.error);
+    expect(response.status).toEqual(expectedResponse.status);
+    expect(response.statusText).toEqual(expectedResponse.statusText);
+  }));
+
+  it('addReview() should add valid review', fakeAsync(() => {
+    const culturalOfferId = 1;
+
+    let response: any;
+    service.addReview(mockReviewToAdd, culturalOfferId).subscribe(result => response = result);
+
+    const reviewsUrl = `${offersUrl}/${culturalOfferId}/reviews`;
+    const req = httpMock.expectOne(reviewsUrl);
+    expect(req.request.method).toBe('POST');
+    req.flush({}, { status: 200, statusText: 'Ok' });
+
+    tick();
+
+    expect(response).toBeTruthy();
+  }));
+
+  it('addReview() should throw exception => non existing cultural offer id', fakeAsync(() => {
+    const culturalOfferId = -1;
+
+    const expectedResponse: HttpErrorResponse = new HttpErrorResponse({
+      error: 'Cultural offer with id -1 does not exist',
+      status: 404,
+      statusText: 'Not found'
+    });
+
+    let response: any;
+    service.addReview(mockReviewToAdd, culturalOfferId).subscribe(result => response = result);
+
+    const reviewsUrl = `${offersUrl}/${culturalOfferId}/reviews`;
+    const req = httpMock.expectOne(reviewsUrl);
+    expect(req.request.method).toBe('POST');
+    req.flush(expectedResponse);
+
+    tick();
+
+    expect(response.error).toEqual(expectedResponse.error);
+    expect(response.status).toEqual(expectedResponse.status);
+    expect(response.statusText).toEqual(expectedResponse.statusText);
+  }));
+
+  it('addReview() should throw exception => empty comment', fakeAsync(() => {
+    const culturalOfferId = 1;
+
+    const expectedResponse: HttpErrorResponse = new HttpErrorResponse({
+      error: 'Comment must not be empty',
+      status: 400,
+      statusText: 'Bad request'
+    });
+
+    let response: any;
+    service.addReview(mockEmptyCommentReviewToAdd, culturalOfferId).subscribe(result => response = result);
+
+    const reviewsUrl = `${offersUrl}/${culturalOfferId}/reviews`;
+    const req = httpMock.expectOne(reviewsUrl);
+    expect(req.request.method).toBe('POST');
+    req.flush(expectedResponse);
+
+    tick();
+
+    expect(response.error).toEqual(expectedResponse.error);
+    expect(response.status).toEqual(expectedResponse.status);
+    expect(response.statusText).toEqual(expectedResponse.statusText);
+  }));
+
+  it('addReview() should throw exception => negative rating', fakeAsync(() => {
+    const culturalOfferId = 1;
+
+    const expectedResponse: HttpErrorResponse = new HttpErrorResponse({
+      error: 'Rating must be positive number',
+      status: 400,
+      statusText: 'Bad request'
+    });
+
+    let response: any;
+    service.addReview(mockNegativeRatingReviewToAdd, culturalOfferId).subscribe(result => response = result);
+
+    const reviewsUrl = `${offersUrl}/${culturalOfferId}/reviews`;
+    const req = httpMock.expectOne(reviewsUrl);
+    expect(req.request.method).toBe('POST');
+    req.flush(expectedResponse);
+
+    tick();
+
+    expect(response.error).toEqual(expectedResponse.error);
+    expect(response.status).toEqual(expectedResponse.status);
+    expect(response.statusText).toEqual(expectedResponse.statusText);
   }));
 });
--- culture-ui/src/app/shared/testing/mock-data.ts
+++ culture-ui/src/app/shared/testing/mock-data.ts
@@ -2,8 +2,9 @@ import { Role } from 'src/app/modules/authentication/role';
 import { User } from 'src/app/modules/authentication/user';
 import { CategoriesPage, Category } from 'src/app/modules/categories/category';
 import { CulturalOffer, CulturalOfferLocation, LocationRange } from 'src/app/modules/cultural-offers/cultural-offer';
+import { SubscribedOfferPage, SubscribedSubcategoriesPage } from 'src/app/modules/dashboard/subscriptions-details';
 import { News, NewsPage } from 'src/app/modules/news/news';
-import { Review, ReviewPage } from 'src/app/modules/reviews/review';
+import { ReplyToAdd, Review, ReviewPage, ReviewToAdd } from 'src/app/modules/reviews/review';
 import { Subcategory } from 'src/app/modules/subcategories/subcategory';
 
 const mockCategoryNames: Category[] = [
@@ -31,127 +32,170 @@ const mockSubcategoryNames: Subcategory[] = [
 ];
 
 const mockCulturalOffer: CulturalOffer = {
-    id: 1,
-    name: 'Offer 1',
-    description: 'Some description',
-    rating: 3.0,
-    reviewCount: 2,
-    images: ['1', '2'],
-    latitude: 45.0,
-    longitude: 45.0
+  id: 1,
+  name: 'Offer 1',
+  description: 'Some description',
+  rating: 3.0,
+  reviewCount: 2,
+  images: ['1', '2'],
+  latitude: 45.0,
+  longitude: 45.0
 };
 
 const mockOfferLocations: CulturalOfferLocation[] = [
-    {
-      id: 1,
-      name: 'Offer 1',
-      location: {
-        latitude: 40.0,
-        longitude: 40.0,
-        address: 'Address 1'
-      }
-    },
-    {
-      id: 2,
-      name: 'Offer 2',
-      location: {
-        latitude: 41.0,
-        longitude: 41.0,
-        address: 'Address 2'
-      }
+  {
+    id: 1,
+    name: 'Offer 1',
+    location: {
+      latitude: 40.0,
+      longitude: 40.0,
+      address: 'Address 1'
     }
-  ];
+  },
+  {
+    id: 2,
+    name: 'Offer 2',
+    location: {
+      latitude: 41.0,
+      longitude: 41.0,
+      address: 'Address 2'
+    }
+  }
+];
 
 const mockNews: News = {
+  id: 1,
+  culturalOfferId: 1,
+  title: 'Some title',
+  postedDate: [1970, 1, 1],
+  author: {
     id: 1,
-    culturalOfferId: 1,
-    title: 'Some title',
-    postedDate: [1970, 1, 1],
-    author: {
+    firstName: 'First name author',
+    lastName: 'Last name author'
+  },
+  text: 'Some text',
+  images: ['image1', 'image2']
+};
+
+const mockNewsPage: NewsPage = {
+  data: [
+    {
+      id: 1,
+      culturalOfferId: 1,
+      title: 'Some title',
+      postedDate: [1970, 1, 1],
+      author: {
         id: 1,
         firstName: 'First name author',
         lastName: 'Last name author'
+      },
+      text: 'Some text',
+      images: ['image1', 'image2'],
+      imagesIds: [1, 2]
     },
-    text: 'Some text',
-    images: ['image1', 'image2']
-    };
-
-const mockNewsPage: NewsPage = {
-    data: [
-        {
-        id: 1,
-        culturalOfferId: 1,
-        title: 'Some title',
-        postedDate: [1970, 1, 1],
-        author: {
-            id: 1,
-            firstName: 'First name author',
-            lastName: 'Last name author'
-        },
-        text: 'Some text',
-        images: ['image1', 'image2'],
-        imagesIds: [1, 2]
-        },
-        {
+    {
+      id: 2,
+      culturalOfferId: 1,
+      title: 'Some title 2',
+      postedDate: [1970, 1, 1],
+      author: {
         id: 2,
-        culturalOfferId: 1,
-        title: 'Some title 2',
-        postedDate: [1970, 1, 1],
-        author: {
-            id: 2,
-            firstName: 'First name author 2',
-            lastName: 'Last name author 2'
-        },
-        text: 'Some other text',
-        images: ['image1', 'image2'],
-        imagesIds: [1, 2]
-        }
-    ],
-    links: new Map([['next', 'next-link'], ['self', 'self-link']])
+        firstName: 'First name author 2',
+        lastName: 'Last name author 2'
+      },
+      text: 'Some other text',
+      images: ['image1', 'image2'],
+      imagesIds: [1, 2]
+    }
+  ],
+  links: new Map([['next', 'next-link'], ['self', 'self-link']])
 };
 
-const mockReview: Review =  {
+const mockReview: Review = {
+  id: 1,
+  culturalOfferId: 1,
+  rating: 3.0,
+  author: {
     id: 1,
-    culturalOfferId: 1,
-    rating: 3.0,
-    author: {
-      id: 1,
-      firstName: 'First name author',
-      lastName: 'Last name author'
-    },
-    comment: 'Some text',
-    images: ['image1', 'image2']
-  };
+    firstName: 'First name author',
+    lastName: 'Last name author'
+  },
+  comment: 'Some text',
+  images: ['image1', 'image2']
+};
+
+const mockReplyToAdd: ReplyToAdd = {
+  comment: 'Some comment'
+};
+
+const mockEmptyCommentReplyToAdd: ReplyToAdd = {
+  comment: ''
+};
+
+const mockReviewToAdd: ReviewToAdd = {
+  comment: 'Some comment',
+  rating: 5,
+  images: [
+    1,
+    2,
+    3
+  ]
+};
+
+const mockEmptyCommentReviewToAdd: ReviewToAdd = {
+  comment: '',
+  rating: 5,
+  images: [
+    1,
+    2,
+    3
+  ]
+};
+
+const mockNegativeRatingReviewToAdd: ReviewToAdd = {
+  comment: 'Some comment',
+  rating: -5,
+  images: [
+    1,
+    2,
+    3
+  ]
+};
+
+const mockEmptyReviewPage: ReviewPage = {
+  data: [],
+  links: new Map([['prev', 'prev-link'], ['self', 'self-link']])
+};
 
 const mockReviewPage: ReviewPage = {
-    data: [
-      {
+  data: [
+    {
+      id: 1,
+      culturalOfferId: 1,
+      rating: 3.0,
+      author: {
         id: 1,
-        culturalOfferId: 1,
-        rating: 3.0,
-        author: {
-          id: 1,
-          firstName: 'First name author',
-          lastName: 'Last name author'
-        },
-        comment: 'Some text',
-        images: ['image1', 'image2']
+        firstName: 'First name author',
+        lastName: 'Last name author'
       },
-      {
+      comment: 'Some text',
+      images: ['image1', 'image2']
+    },
+    {
+      id: 2,
+      culturalOfferId: 1,
+      rating: 4.0,
+      author: {
         id: 2,
-        culturalOfferId: 1,
-        rating: 4.0,
-        author: {
-          id: 2,
-          firstName: 'First name author 2',
-          lastName: 'Last name author 2'
-        },
-        comment: 'Some other text',
-        images: ['image1', 'image2']
-      }
-    ],
-    links: new Map([['next', 'next-link'], ['self', 'self-link']])
-  };
+        firstName: 'First name author 2',
+        lastName: 'Last name author 2'
+      },
+      comment: 'Some other text',
+      images: ['image1', 'image2']
+    }
+  ],
+  links: new Map([['next', 'next-link'], ['self', 'self-link']])
+};
 
 const mockCategoriesPage: CategoriesPage = {
   data: [
@@ -190,7 +234,7 @@ const invalidLocation = 'Not valid';
 const invalidLocationMessage = { severity: 'warn', summary: 'Error!', detail: 'Location not found' };
 
 const mockMapBounds: google.maps.LatLngBounds =
-  new google.maps.LatLngBounds({lat: 30.0, lng: 30.0}, {lat: 45.0, lng: 45.0});
+  new google.maps.LatLngBounds({ lat: 30.0, lng: 30.0 }, { lat: 45.0, lng: 45.0 });
 
 const mockLocationRange: LocationRange = {
   latitudeFrom: 30.0,
@@ -198,6 +242,68 @@ const mockLocationRange: LocationRange = {
   latitudeTo: 45.0,
   longitudeTo: 45.0
 };
+
+
+const mockSubscribedSubcategoriesPage: SubscribedSubcategoriesPage = {
+  data: [
+    {
+      id: 1,
+      categoryId: 1,
+      name: 'Some name'
+    },
+    {
+      id: 2,
+      categoryId: 1,
+      name: 'Some name 2'
+    }
+  ],
+  links: new Map([['next', 'next-link'], ['self', 'self-link']])
+};
+
+const mockEmptySubscribedSubcategoriesPage: SubscribedSubcategoriesPage = {
+  data: [],
+  links: new Map([['prev', 'prev-link'], ['self', 'self-link']])
+};
+
+const mockSubscribedOffersPage: SubscribedOfferPage = {
+  data: [
+    {
+      id: 1,
+      name: 'Some name',
+      description: 'Some description',
+      images: [
+        'some image link',
+        'some image link',
+        'some image link',
+      ]
+    },
+    {
+      id: 2,
+      name: 'Some name 2',
+      description: 'Some description 2',
+      images: [
+        'some image link 2',
+        'some image link 2',
+        'some image link 2',
+      ]
+    }
+  ],
+  links: new Map([['next', 'next-link'], ['self', 'self-link']])
+};
+
+const mockEmptySubscribedOffersPage: SubscribedOfferPage = {
+  data: [],
+  links: new Map([['prev', 'prev-link'], ['self', 'self-link']])
+};
+
+const mockImage1: File = new File([''], 'image1.png');
+const mockImage2: File = new File([''], 'image2.png');
+
+const mockImagesToAdd: FormData = new FormData();
+mockImagesToAdd.append('images', mockImage1);
+mockImagesToAdd.append('images', mockImage2);
+
+const mockEmptyImagesToAdd: FormData = new FormData();
 
 export {
   invalidLocationMessage,
@@ -209,11 +315,24 @@ export {
   mockOfferLocations,
   mockNews,
   mockReview,
+  mockReplyToAdd,
+  mockEmptyCommentReplyToAdd,
   mockNewsPage,
   mockReviewPage,
+  mockEmptyReviewPage,
+  mockReviewToAdd,
+  mockEmptyCommentReviewToAdd,
+  mockNegativeRatingReviewToAdd,
   mockUser,
   mockAdmin,
   mockMapBounds,
   mockLocationRange,
-  mockCategoriesPage
+  mockCategoriesPage,
+
+  mockSubscribedSubcategoriesPage,
+  mockEmptySubscribedSubcategoriesPage,
+  mockSubscribedOffersPage,
+  mockEmptySubscribedOffersPage,
+  mockImagesToAdd,
+  mockEmptyImagesToAdd
 };
